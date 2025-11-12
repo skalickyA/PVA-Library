f57a78ab785ef88cbbbdc150f738b6a07bf38e15
SkalickýAleš_4wrtv7e <a.skalicky05@gmail.com>
2025-11-12 10:25:28 +0100

    a

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -6,42 +6,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Library {
-    private ArrayList<Entita> entitaArrayList;
+
+    private BookManager bookManager;
 
     public Library() {
-        this.entitaArrayList = new ArrayList<>();
+        this.bookManager = new BookManager();
     }
 
-    public void loadData(){
-        String csvSoubor = "C:\\Users\\SkalickýAleš_4wrtv7e\\Downloads\\untitled10\\src\\main\\java\\org\\example\\test.csv";
-        String radek;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(csvSoubor))) {
-            br.readLine();
-            while ((radek = br.readLine()) != null) {
-                String[] hodnoty = radek.split(",");
-                String jmeno = hodnoty[0];
-                String prijmeni = hodnoty[1];
-                int vek = Integer.parseInt(hodnoty[2]);
-
-                entitaArrayList.add(new Entita(jmeno, prijmeni, vek));
-            }
-        } catch (IOException e) {
-            System.out.println("Chyba při čtení souboru: " + e.getMessage());
-        }
-    }
-
-    public void printData(){
-        for (Entita x: entitaArrayList){
-            System.out.println(x);
-        }
-    }
-
-    public void ageFilter(){
-        for (Entita x: entitaArrayList){
-            if (x.getAge() >= 18){
-                System.out.println(x);
-            }
-        }
-    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,9 +11,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BookManager bookManager = new BookManager();
-        bookManager.loadBookData();
-        bookManager.printBookData();
+        Library library = new Library();
+
+
+
 
 
     }
