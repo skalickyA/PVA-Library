96b5b6857fe81f3634adb2b5e92a422ea5a94359
SkalickýAleš_4wrtv7e <a.skalicky05@gmail.com>
2025-11-12 09:59:03 +0100

    a

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,14 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Customer extends Entita {
     private double money;
+    private int id;
+
 
 
     public Customer(String name, String lastName, int age) {
         super(name, lastName, age);
     }
 
-    public void ahoj(){
-        System.out.println("Ahoj");
-    }
 }
