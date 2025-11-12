7000de7f8aaa799d83b1c7636792d1362bd9d6bb
SkalickýAleš_4wrtv7e <a.skalicky05@gmail.com>
2025-11-12 10:03:51 +0100

    a

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -5,11 +5,21 @@ import java.util.ArrayList;
 public class Customer extends Entita {
     private double money;
     private int id;
+    private ArrayList<Book> bookArrayList;
 
-
+    public Customer(String name, String lastName, int age, double money, int id) {
+        super(name, lastName, age);
+        this.money = money;
+        this.id = id;
+        this.bookArrayList = new ArrayList<>();
+    }
 
     public Customer(String name, String lastName, int age) {
         super(name, lastName, age);
     }
 
+    public void printDataCustomer(){
+
+        System.out.println("Name: " + getName() + " LastName" + getLastName() + " Age: " + getAge());
+    }
 }
