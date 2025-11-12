94d58fa2acb3ee5187ba001cee7b88d6264a37e1
SkalickýAleš_4wrtv7e <a.skalicky05@gmail.com>
2025-11-12 10:09:31 +0100

    a

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,13 +1,21 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Book {
     private String name;
+    private int id;
 
-    public Book(String name) {
+    public Book(String name, int id) {
         this.name = name;
+        this.id = id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getId() {
+        return id;
     }
 }
---
+++ src/main/java/org/example/BookManager.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class BookManager {
+    private static ArrayList<Book> bookArrayList;
+
+
+}
