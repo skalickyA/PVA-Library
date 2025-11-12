a0a832c23ef35fce305834fa03d34d07b1bec948
SkalickýAleš_4wrtv7e <a.skalicky05@gmail.com>
2025-11-12 11:00:02 +0100

    a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,20 +11,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         Library library = new Library();
         library.loadBookData();
         library.printBookData();
 
         library.loadEmployeeData();
         library.printEmployeeData();
-
-
-
-
-
-
-
 
     }
 }
